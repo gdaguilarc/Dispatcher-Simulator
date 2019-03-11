6fe2145a752a25997124220f519c994673bf86e9
David Aguilar <gd.aguilarc@gmail.com>
2019-03-11 16:22:42 -0600

    Basic classes

--- dispatcher.js
+++ dispatcher.js
@@ -1,14 +1,53 @@
 // Initialization of all the Micros
 // TODO: Use args and chalk to communicate with the terminal
-initMicros(2);
+// initMicros(2);
 
-function initMicros(numberOfMicros) {
-  let micros = [numberOfMicros - 1];
-  var i;
-  for (i = 0; i < numberOfMicros; i++) {
-    micros[i] = {
-      total: 0,
-      processes: []
-    };
+// function initMicros(numberOfMicros) {
+//   let micros = [numberOfMicros - 1];
+//   var i;
+//   for (i = 0; i < numberOfMicros; i++) {
+//     micros[i] = {
+//       total: 0,
+//       processes: []
+//     };
+//   }
+// }
+
+const Process = require('./Process');
+
+class Dispatcher {
+  constructor() {
+    this.micros = [];
+    this.data = [];
+  }
+  solve(n, data) {
+    this.initMicros(n);
+    this.compute(data);
+  }
+  initMicros(n) {
+    for (let i = 0; i < n; i++) {
+      this.micros[i].push({ total: 0, tasks: [] });
+    }
+  }
+
+  compute(data) {}
+
+  dataParsing(data) {
+    data.sort((a, b) => {
+      return parseInt(a.herarchy) - parseInt(b.herarchy);
+    });
+
+    for (let i = 0; i < data.length; i++) {
+      let temp = new Process(
+        data[i].herarchy,
+        data[i].name,
+        data[i].te,
+        data[i].block,
+        data[i].ready_time
+      );
+      this.data.push(temp);
+    }
   }
 }
+
+module.exports = Dispatcher;
--- index.js
+++ index.js
@@ -1,24 +1,9 @@
-// Importing file System to write the data.json
-const fs = require('fs');
+const Dispatcher = require('./dispatcher');
+const data = require('./data');
+const chalk = require('chalk');
 
-// Columns inside our data.csv
-const columns = ['herarchy', 'name', 'te', 'block', 'ready_time'];
+let dispatcher = new Dispatcher();
+dispatcher.dataParsing(data);
 
-// Deleting old file if exist
-fs.exists('./data/data.json', exist => {
-  if (exist) fs.unlinkSync('./data/data.json');
-});
-
-// Transformation of the csv to the JSON array
-require('csv-to-array')(
-  {
-    file: './data/input.csv',
-    columns: columns
-  },
-  function(err, array) {
-    fs.appendFileSync('./data/data.json', JSON.stringify(array));
-  }
-);
-
-// Adquiring our data from the new file
-const data = require('./data/data.json');
+console.log(chalk.bgRed.white.bold('Dispatcher: \n'), dispatcher);
+console.log(chalk.bgRed.white.bold('First process: \n'), dispatcher.data[0]);
