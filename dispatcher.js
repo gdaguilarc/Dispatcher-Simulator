904041624c39f13f91675bece5b0136ff4fb8592
David Aguilar <gd.aguilarc@gmail.com>
2019-03-11 19:50:29 -0600

    TODO: Compute method

--- dispatcher.js
+++ dispatcher.js
@@ -1,18 +1,3 @@
-// Initialization of all the Micros
-// TODO: Use args and chalk to communicate with the terminal
-// initMicros(2);
-
-// function initMicros(numberOfMicros) {
-//   let micros = [numberOfMicros - 1];
-//   var i;
-//   for (i = 0; i < numberOfMicros; i++) {
-//     micros[i] = {
-//       total: 0,
-//       processes: []
-//     };
-//   }
-// }
-
 const Process = require('./Process');
 
 class Dispatcher {
@@ -30,6 +15,7 @@ class Dispatcher {
     }
   }
 
+  // TODO: Compute method
   compute(data) {}
 
   dataParsing(data) {
